b61700b2d08c154548f6508721e3503d8734a877
hi-jie <3318897344>
2022-01-28 15:43:31 +0800

    更新

---
+++ js/index.js
@@ -0,0 +1,14 @@
+$('#login-button').click(function (event) {
+	var userName=document.getElementById("userName").value;
+    var pwd=document.getElementById("pwd").value;
+		//修改密码请改此处
+    if(userName=="黄嘉琪" &&  pwd=="0202"){
+			event.preventDefault();
+			$('form').fadeOut(500);
+			$('.wrapper').addClass('form-success');
+			setTimeout(function(){location.href="BirthdayCake.html";},2000);
+		}
+	else{
+		alert("用户名或密码错误！");
+	}
+});
